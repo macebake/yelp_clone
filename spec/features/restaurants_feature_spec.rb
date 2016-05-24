ad4aa21dc37e11e87da7ecf4b79da7086b612b70
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-24 17:01:13 +0100

    displaying restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
-  context 'no restaurants have been added'
+  context 'no restaurants have been added' do
 
     scenario 'displays message when no restaurants have been added' do
       visit '/restaurants'
@@ -9,7 +9,19 @@ feature 'restaurants' do
       expect(page).to have_link 'Add Restaurant'
     end
 
-    scenario ''
+  end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create name:'Michael\'s Crab Shack'
+    end
+
+    scenario 'displays the restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'Michael\'s Crab Shack'
+      expect(page).not_to have_content 'No restaurants found'
+      expect(page).to have_link 'Add Restaurant'
+    end
 
   end
 
